203c4052338a4bc76451cab130ace6c9b77dc3a5
KarthikSJ97 <karthik.s.jhingade@gmail.com>
2026-07-10 13:17:08 -0700

    Pattern printing: Pattern3

---
+++ patterns/Pattern3.java
@@ -0,0 +1,21 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern3(n);
+        sc.close();
+    }
+
+    public static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}

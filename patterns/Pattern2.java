cddf102975db2f13e25d03a5b4e669cd6b1e5280
KarthikSJ97 <karthik.s.jhingade@gmail.com>
2026-07-10 13:14:43 -0700

    Pattern printing: Pattern2

---
+++ patterns/Pattern2.java
@@ -0,0 +1,21 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern2(n);
+        sc.close();
+    }
+
+    public static void pattern2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

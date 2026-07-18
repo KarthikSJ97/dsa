2ba119b24c72b185d5058ca48f231f8bfb605d87
KarthikSJ97 <karthik.s.jhingade@gmail.com>
2026-07-18 13:48:57 -0700

    Pattern printing: Pattern13

---
+++ patterns/Pattern13.java
@@ -0,0 +1,24 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern13(n);
+        sc.close();
+    }
+
+    public static void pattern13(int n) {
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+}
